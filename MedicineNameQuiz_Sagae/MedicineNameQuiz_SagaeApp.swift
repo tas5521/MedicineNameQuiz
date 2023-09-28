eeeece7d83b01e2ad247d92b7bd4dcc26925c814
tas5521 <tsbs11152@gmail.com>
2023-09-28 15:51:40 +0900

    薬の名前クイズアプリを追加

---
+++ MedicineNameQuiz_Sagae/MedicineNameQuiz_SagaeApp.swift
@@ -0,0 +1,20 @@
+//
+//  MedicineNameQuiz_SagaeApp.swift
+//  MedicineNameQuiz_Sagae
+//
+//  Created by 寒河江彪流 on 2023/09/28.
+//
+
+import SwiftUI
+
+@main
+struct MedicineNameQuiz_SagaeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
