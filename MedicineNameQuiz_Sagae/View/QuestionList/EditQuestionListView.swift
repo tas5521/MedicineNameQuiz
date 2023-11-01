47cfa102a0211bb1e76ada4dc18068ed98680f03
tas5521 <tsbs11152@gmail.com>
2023-11-01 19:04:34 +0900

    #2 EditQuestionListViewのViewの分割を再考

--- MedicineNameQuiz_Sagae/View/QuestionList/EditQuestionListView.swift
+++ MedicineNameQuiz_Sagae/View/QuestionList/EditQuestionListView.swift
@@ -12,19 +12,21 @@ struct EditQuestionListView: View {
     @Environment(\.dismiss) private var dismiss
     // タブの選択項目を保持する変数
     @State private var tabIndex: Int = 0
-
+    
     var body: some View {
         // 垂直右方向にレイアウト
         VStack {
             // 薬の区分を選択するタブを配置
-            classificationTab
+            TopTabView(
+                tabNameList: MedicineClassification.allCases.map({classification in classification.rawValue}),
+                tabIndex: $tabIndex)
             // リスト名編集用テキストフィールド
-            listNameEditTextField
-            // 薬を検索するためのテキストフィールド
-            medicineSearchBar
+            Text("リスト名編集用テキストフィールド")
+            // 薬を検索バー
+            Text("薬の検索バー")
             Spacer()
             // 薬リスト
-            medicineList
+            Text("薬リスト")
             Spacer()
         } // VStack ここまで
         // ナビゲーションバータイトルを指定
@@ -46,29 +48,6 @@ struct EditQuestionListView: View {
             } // ToolbarItem ここまで
         } // toolbar ここまで
     } // body ここまで
-
-    // 薬の区分を選択するタブ
-    private var classificationTab: some View {
-        // 薬の区分の配列を取得
-        let classificationArray = MedicineClassification.allCases.map({classification in classification.rawValue})
-        // 薬の区分を選択するタブを返す
-        return TopTabView(tabNameList: classificationArray, tabIndex: $tabIndex)
-    } // classificationTab ここまで
-    
-    // リスト名編集用テキストフィールド
-    private var listNameEditTextField: some View {
-        Text("リスト名編集用テキストフィールド")
-    } // listNameEditTextField ここまで
-    
-    // 薬を検索するためのテキストフィールド
-    private var medicineSearchBar: some View {
-        Text("薬の検索バー")
-    } // medicineSearchBar ここまで
-
-    // 薬のリスト
-    private var medicineList: some View {
-        Text("薬リスト")
-    } // medicineList ここまで
 } // EditQuestionListViewここまで
 
 #Preview {
