352e2cea5c33bff2aaf1a0c44b1a2847ae5ee679
tas5521 <tsbs11152@gmail.com>
2023-12-22 01:14:15 +0900

    #16 ダミーの配列を改善

--- MedicineNameQuiz_Sagae/View/QuestionList/CreateQuestionListView.swift
+++ MedicineNameQuiz_Sagae/View/QuestionList/CreateQuestionListView.swift
@@ -28,30 +28,30 @@ struct CreateQuestionListView: View {
     
     // ダミーの内用薬の配列
     @State private var dummyInternalMedicineList: [MedicineListItem] = [
-        MedicineListItem(medicineClassification: .internalMedicine, originalName: "内用薬先発品名1", genericName: "内用薬一般名1", checked: false),
-        MedicineListItem(medicineClassification: .internalMedicine, originalName: "内用薬先発品名2", genericName: "内用薬一般名2", checked: false),
-        MedicineListItem(medicineClassification: .internalMedicine, originalName: "内用薬先発品名3", genericName: "内用薬一般名3", checked: false)
+        MedicineListItem(originalName: "内用薬先発品名1", genericName: "内用薬一般名1", checked: false),
+        MedicineListItem(originalName: "内用薬先発品名2", genericName: "内用薬一般名2", checked: false),
+        MedicineListItem(originalName: "内用薬先発品名3", genericName: "内用薬一般名3", checked: false)
     ] // dummyInternalMedicineList ここまで
     
     // ダミーの注射薬の配列
     @State private var dummyInjectionMedicineList: [MedicineListItem] = [
-        MedicineListItem(medicineClassification: .injectionMedicine, originalName: "注射薬先発品名1", genericName: "注射薬一般名1", checked: false),
-        MedicineListItem(medicineClassification: .injectionMedicine, originalName: "注射薬先発品名2", genericName: "注射薬一般名2", checked: false),
-        MedicineListItem(medicineClassification: .injectionMedicine, originalName: "注射薬先発品名3", genericName: "注射薬一般名3", checked: false)
+        MedicineListItem(originalName: "注射薬先発品名1", genericName: "注射薬一般名1", checked: false),
+        MedicineListItem(originalName: "注射薬先発品名2", genericName: "注射薬一般名2", checked: false),
+        MedicineListItem(originalName: "注射薬先発品名3", genericName: "注射薬一般名3", checked: false)
     ] // dummyInjectionMedicineList ここまで
     
     // ダミーの外用薬の配列
     @State private var dummyExternalMedicineList: [MedicineListItem] = [
-        MedicineListItem(medicineClassification: .externalMedicine, originalName: "外用薬先発品名1", genericName: "外用薬一般名1", checked: false),
-        MedicineListItem(medicineClassification: .externalMedicine, originalName: "外用薬先発品名2", genericName: "外用薬一般名2", checked: false),
-        MedicineListItem(medicineClassification: .externalMedicine, originalName: "外用薬先発品名3", genericName: "外用薬一般名3", checked: false)
+        MedicineListItem(originalName: "外用薬先発品名1", genericName: "外用薬一般名1", checked: false),
+        MedicineListItem(originalName: "外用薬先発品名2", genericName: "外用薬一般名2", checked: false),
+        MedicineListItem(originalName: "外用薬先発品名3", genericName: "外用薬一般名3", checked: false)
     ] // dummyExternalMedicineList ここまで
     
     // ダミーのカスタム薬の配列
     @State private var dummyCustomMedicineList: [MedicineListItem] = [
-        MedicineListItem(medicineClassification: .customMedicine, originalName: "カスタム先発品名1", genericName: "カスタム一般名1", checked: false),
-        MedicineListItem(medicineClassification: .customMedicine, originalName: "カスタム先発品名2", genericName: "カスタム一般名2", checked: false),
-        MedicineListItem(medicineClassification: .customMedicine, originalName: "カスタム先発品名3", genericName: "カスタム一般名3", checked: false)
+        MedicineListItem(originalName: "カスタム先発品名1", genericName: "カスタム一般名1", checked: false),
+        MedicineListItem(originalName: "カスタム先発品名2", genericName: "カスタム一般名2", checked: false),
+        MedicineListItem(originalName: "カスタム先発品名3", genericName: "カスタム一般名3", checked: false)
     ] // dummyCustomMedicineList ここまで
         
     // View Presentation State
