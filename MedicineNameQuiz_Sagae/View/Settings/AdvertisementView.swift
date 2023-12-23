3c44483bcf74d2ac0af0e5c0ef441cc4807812e2
tas5521 <tsbs11152@gmail.com>
2023-12-24 02:04:38 +0900

    #28 NavigationBarの外観を変更

--- MedicineNameQuiz_Sagae/View/Settings/AdvertisementView.swift
+++ MedicineNameQuiz_Sagae/View/Settings/AdvertisementView.swift
@@ -22,6 +22,12 @@ struct AdvertisementView: View {
         } // ZStack ここまで
         // ナビゲーションバータイトルを指定
         .navigationBarTitle(title, displayMode: .inline)
+        // ナビゲーションバーの背景を青色に変更
+        .toolbarBackground(.navigationBarBlue, for: .navigationBar)
+        // ナビゲーションバーの背景を表示
+        .toolbarBackground(.visible, for: .navigationBar)
+        // ナビゲーションバーのタイトルの色を白にする
+        .toolbarColorScheme(.dark)
     } // body ここまで
 } // AdvertisementView ここまで
 
