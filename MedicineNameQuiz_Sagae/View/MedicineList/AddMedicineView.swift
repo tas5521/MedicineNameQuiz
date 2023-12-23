4310709d0c709a62e1ea0e98c24915b359d8c1f7
tas5521 <tsbs11152@gmail.com>
2023-12-23 23:54:57 +0900

    #18 ボタンの外観を変更

--- MedicineNameQuiz_Sagae/View/MedicineList/AddMedicineView.swift
+++ MedicineNameQuiz_Sagae/View/MedicineList/AddMedicineView.swift
@@ -14,7 +14,7 @@ struct AddMedicineView: View {
     @State private var brandNameText: String = ""
     // 入力された一般名を管理する変数
     @State private var genericNameText: String = ""
-
+    
     
     var body: some View {
         // 奥から手前にレイアウト
@@ -25,6 +25,9 @@ struct AddMedicineView: View {
                 .ignoresSafeArea()
             // 垂直方向にレイアウト
             VStack {
+                // スペースを空ける
+                Spacer()
+
                 // 追加する薬の名前を入力するようユーザーに促すためのテキスト
                 Text("追加する薬の名前を入力してください")
                     .padding()
@@ -36,6 +39,8 @@ struct AddMedicineView: View {
                 TextField("一般名", text: $genericNameText)
                     .textFieldStyle(.roundedBorder)
                     .padding()
+                // スペースを空ける
+                Spacer()
                 // 追加ボタン
                 Button {
                     // 薬の名前をカスタムに追加する処理
@@ -43,8 +48,14 @@ struct AddMedicineView: View {
                 } label: {
                     // ラベル
                     Text("追加")
+                    // 幅300高さ50に指定
+                        .frame(width: 300, height: 60)
+                    // 背景色をオレンジに指定
+                        .background(Color.buttonOrange)
+                    // 角を丸くする
+                        .clipShape(.buttonBorder)
                 } // Button ここまで
-                    .padding()
+                .padding()
                 // やめるボタン
                 Button {
                     // シートを閉じる
@@ -52,8 +63,17 @@ struct AddMedicineView: View {
                 } label: {
                     // ラベル
                     Text("やめる")
+                    // 幅300高さ50に指定
+                        .frame(width: 300, height: 60)
+                    // 背景色をオレンジに指定
+                        .background(Color.buttonOrange)
+                    // 角を丸くする
+                        .clipShape(.buttonBorder)
                 } // Button ここまで
-                    .padding()
+                .padding()
+                // スペースを空ける
+                Spacer()
+
             } // VStack ここまで
         } // ZStack ここまで
     } // body ここまで
