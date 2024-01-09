f31a124bc576c98834e9b408b91f08ab5b938a11
tas5521 <tsbs11152@gmail.com>
2024-01-09 23:50:27 +0900

    #15 リスト名編集用テキストフィールドの外観を変更

--- MedicineNameQuiz_Sagae/View/QuestionList/EditQuestionListView.swift
+++ MedicineNameQuiz_Sagae/View/QuestionList/EditQuestionListView.swift
@@ -73,8 +73,16 @@ struct EditQuestionListView: View {
                     tabIndex: $tabIndex, tabNameList: MedicineClassification.allCases.map({classification in classification.rawValue}))
                 // リスト名編集用テキストフィールド
                 TextField("リストの名前を入力してください", text: $listName)
-                // 丸い縁をつける
-                    .textFieldStyle(.roundedBorder)
+                // 文字の左に余白を追加
+                    .padding(.leading, 6)
+                // 背景色を指定
+                    .background(
+                        // 背景を角の丸い四角にする
+                        RoundedRectangle(cornerRadius: 8)
+                        // 背景色を変更
+                            .fill(Color(red: 237/255, green: 237/255, blue: 237/255))
+                        // 高さを36に指定
+                            .frame(height: 36))
                 // 上下左右に余白をつける
                     .padding()
                 // 薬の検索バーを配置
