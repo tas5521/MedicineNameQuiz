6b201207f27cbd305a7d03b927732f72efdb98f9
tas5521 <tsbs11152@gmail.com>
2024-01-17 22:24:35 +0900

    #18 先発品名のテキストの変数名を、brandNameTextからoriginalNameTextに変更

--- MedicineNameQuiz_Sagae/View/MedicineList/AddMedicineView.swift
+++ MedicineNameQuiz_Sagae/View/MedicineList/AddMedicineView.swift
@@ -11,7 +11,7 @@ struct AddMedicineView: View {
     // 画面を閉じるために用いる環境変数
     @Environment(\.dismiss) private var dismiss
     // 入力された先発品名を管理する変数
-    @State private var brandNameText: String = ""
+    @State private var originalNameText: String = ""
     // 入力された一般名を管理する変数
     @State private var genericNameText: String = ""
     
@@ -36,7 +36,7 @@ struct AddMedicineView: View {
                 // 上下左右に余白を追加
                     .padding()
                 // 先発品名を入力するためのテキストフィールド
-                TextField("先発品名", text: $brandNameText)
+                TextField("先発品名", text: $originalNameText)
                 // テキストフィールドの背景を指定
                     .textFieldBackground()
                 // 上下左右に余白を追加
@@ -59,13 +59,13 @@ struct AddMedicineView: View {
                     // 幅300高さ50に指定
                         .frame(width: 300, height: 60)
                     // 両方のテキストフィールドに文字が入力されている場合、背景色をオレンジに指定。そうでない場合はグレー
-                        .background(brandNameText == "" || genericNameText == "" ? Color.disabledButtonGray : Color.buttonOrange)
+                        .background(originalNameText == "" || genericNameText == "" ? Color.disabledButtonGray : Color.buttonOrange)
                     // 文字色を白に指定
                         .foregroundStyle(Color.white)
                     // 角を丸くする
                         .clipShape(.buttonBorder)
                 } // Button ここまで
-                .disabled(brandNameText == "" || genericNameText == "")
+                .disabled(originalNameText == "" || genericNameText == "")
                 // 太字にする
                 .bold()
                 // 下に余白を追加
