d3f56c6a97fb3c1e599e9c80d13bc6f71197a66e
tas5521 <tsbs11152@gmail.com>
2024-02-23 01:24:09 +0900

    #61 CSVの読み込みを担当するクラスCSVLoaderを作成

--- MedicineNameQuiz_Sagae/Model/MedicineClassification.swift
+++ MedicineNameQuiz_Sagae/Model/MedicineClassification.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import CoreData
 
 enum MedicineClassification: String, CaseIterable {
     case internalMedicine = "内用薬"
@@ -21,8 +20,9 @@ enum MedicineClassification: String, CaseIterable {
     var medicineNameData: [MedicineItem] {
         // もし薬データを読み込んでいなかったら
         if MedicineClassification.medicineDataArray.isEmpty {
+            let csvLoader = CSVLoader()
             // 全ての薬データのCSVLineを取得
-            let medicineDataCSVLines = self.loadCsvFile(resourceName: "MedicineNameList")
+            let medicineDataCSVLines = csvLoader.loadCsvFile(resourceName: "MedicineNameList")
             // 薬のデータを配列に格納
             MedicineClassification.medicineDataArray = medicineDataCSVLines
             // カンマ（,）で分割した配列を作成
@@ -41,27 +41,4 @@ enum MedicineClassification: String, CaseIterable {
         // 薬の名前の要素の配列を返却
         return filteredMedicineDataArray
     } // medicineNameData ここまで
-
-    // CSVファイルを読み込む関数を作成
-    private func loadCsvFile(resourceName: String) -> [String] {
-        // 読み込んだCSVのデータを格納する配列を宣言
-        var csvLines: [String] = []
-
-        // 読み込み先に、ファイルが存在してるかチェック
-        guard let path = Bundle.main.path(forResource: resourceName, ofType:"csv") else {
-            // 存在しなければ、エラーメッセージを配列に格納して返却
-            return ["csvファイルがないよ"]
-        } // guard let ここまで
-        do {
-            // ファイルが存在していれば、データをString型で読み込み
-            let csvString = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
-            // 改行ごとにデータを分割して配列に格納
-            csvLines = csvString.components(separatedBy: .newlines)
-        } catch let error as NSError {
-            // 何らかのエラーが発生した場合は、エラー内容を返却
-            return ["エラー: \(error)"]
-        } // do-try-catch ここまで
-        // 読み込んだCSVを返却
-        return csvLines
-    } // loadCsvFile ここまで
 } // MedicineClassificationここまで
