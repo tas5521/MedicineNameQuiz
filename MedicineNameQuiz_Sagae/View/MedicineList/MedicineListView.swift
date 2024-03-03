17c03da566c4a932718423137881eb9a45d9691e
tas5521 <tsbs11152@gmail.com>
2024-03-03 14:54:49 +0900

    #67 itemsをsearchedItemsに変更

--- MedicineNameQuiz_Sagae/View/MedicineList/MedicineListView.swift
+++ MedicineNameQuiz_Sagae/View/MedicineList/MedicineListView.swift
@@ -96,7 +96,7 @@ struct MedicineListView: View {
                 // 内用薬、注射薬、外用薬が選択されていたら
             } else {
                 List {
-                    ForEach(viewModel.items) { medicine in
+                    ForEach(viewModel.searchedItems) { medicine in
                         // 垂直方向にレイアウト
                         VStack(alignment: .leading) {
                             // 商品名を表示
