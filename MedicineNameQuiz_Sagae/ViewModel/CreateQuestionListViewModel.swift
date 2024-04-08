e70675d6cc3f6c24835a033ff11ea8c7f82f0548
tas5521 <tsbs11152@gmail.com>
2024-04-09 00:51:57 +0900

    #65 mergeQuestionsを改善

--- MedicineNameQuiz_Sagae/ViewModel/CreateQuestionListViewModel.swift
+++ MedicineNameQuiz_Sagae/ViewModel/CreateQuestionListViewModel.swift
@@ -174,37 +174,26 @@ final class CreateQuestionListViewModel {
     } // mergeQuestionsToListItems ここまで
 
     // 選択されている問題を該当するカテゴリの配列にマージする
-    /// 薬のカテゴリで絞って、薬リストのデータの中に同じ商品名・一般名のデータがあるかどうか探索します。
-    /// 同じ商品名・一般名のデータが見つかった場合、薬リストのデータの該当する問題のselectedプロパティをtrueに変更します。
-    /// 探索は、1つの問題につき、薬リストのデータ内に該当するデータが1件見つかり次第終了します。
-    /// 商品名・一般名の両方が一致するものが複数見つかる可能性もありますが、該当データが1件見つかり次第終了することで、
-    /// 複数選択されるのを回避します。
+    /// 同じ商品名・一般名のデータが見つかり、かつ、選択されていない場合、薬リストのデータの該当する問題のselectedプロパティをtrueに変更します。
     /// もし、商品名・一般名が一致するデータが見つからなかった場合、そのデータは、「過去にCoreDataに保存する時には存在したが、
     /// 今は薬リストのデータに無いデータ」なので、このデータをselectedプロパティをtrueにして薬リストに追加します。
     private func mergeQuestions(to listItems: inout [MedicineListItem], with questions: [MedicineItem]) {
         for question in questions {
-            for index in 0...listItems.count {
-                // questionとカテゴリ・商品名・一般名が一致するlistItemがなかった場合
-                if index == listItems.count {
-                    // その問題を配列に追加する
-                    let additionalQuestion = MedicineListItem(category: question.category,
-                                                              brandName: question.brandName,
-                                                              genericName: question.genericName,
-                                                              selected: true)
-                    listItems.append(additionalQuestion)
-                    // questionとカテゴリ・商品名・一般名が一致するlistItemを探している場合
-                } else {
-                    // listItemとquestionの商品名と一般名が一致したら通過。一致しなければ次のループへ
-                    guard listItems[index].brandName == question.brandName &&
-                            listItems[index].genericName == question.genericName
-                    else { continue }
-                    // まだlistItems内の問題が選択されていなかったら通過。選択されていれば、次のループへ
-                    guard listItems[index].selected == false else { continue }
-                    // 問題を選択された状態にし、ループを終了
-                    listItems[index].selected = true
-                    break
-                } // if ここまで
-            } // for ここまで
+            // 条件に一致する最初の要素を取得
+            if let index = listItems.firstIndex(where: {
+                // 商品名・一般名が一致し、かつ、まだ選択されていない要素を探す
+                $0.brandName == question.brandName && $0.genericName == question.genericName && $0.selected == false
+            }) {
+                // 該当する問題のselectedプロパティをtrueに変更
+                listItems[index].selected = true
+            } else {
+                // 商品名・一般名が一致するデータが見つからなかった場合、このデータをselectedプロパティをtrueにして薬リストに追加
+                let newItem = MedicineListItem(category: question.category,
+                                               brandName: question.brandName,
+                                               genericName: question.genericName,
+                                               selected: true)
+                listItems.append(newItem)
+            } // if let ここまで
         } // for ここまで
     } // mergeQuestions ここまで
 } // CreateQuestionListViewModel ここまで
