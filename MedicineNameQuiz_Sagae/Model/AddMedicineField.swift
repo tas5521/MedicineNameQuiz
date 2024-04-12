7a64f5ecc09560cd3df2fadf668028f0e60586f4
tas5521 <tsbs11152@gmail.com>
2024-04-12 23:15:54 +0900

    #93 フォーカス機能を実装

---
+++ MedicineNameQuiz_Sagae/Model/AddMedicineField.swift
@@ -0,0 +1,11 @@
+//
+//  AddMedicineField.swift
+//  MedicineNameQuiz_Sagae
+//
+//  Created by 寒河江彪流 on 2024/04/12.
+//
+
+enum AddMedicineField: Hashable {
+    case brandName
+    case genericName
+} // AddMedicineField ここまで
