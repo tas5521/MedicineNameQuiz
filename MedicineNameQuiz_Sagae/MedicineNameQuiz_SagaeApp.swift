d5eae3b1ae40a76404cc658342fb2d4ee812a45d
tas5521 <tsbs11152@gmail.com>
2024-05-21 12:33:31 +0900

    #104 Google-Mobile-Ads-SDKの初期化

--- MedicineNameQuiz_Sagae/MedicineNameQuiz_SagaeApp.swift
+++ MedicineNameQuiz_Sagae/MedicineNameQuiz_SagaeApp.swift
@@ -6,6 +6,15 @@
 //
 
 import SwiftUI
+import GoogleMobileAds
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                        GADMobileAds.sharedInstance().start(completionHandler: nil)
+                        return true
+                    }
+} // AppDelegate ここまで
 
 @main
 struct MedicineNameQuiz_SagaeApp: App {
