5d24d6d96edc293d3e3cc08dbb494542cb4d7ef8
tas5521 <tsbs11152@gmail.com>
2024-06-20 22:30:41 +0900

    #111 ランキングに関するものを削除

--- MedicineNameQuiz_Sagae/View/Settings/SettingsListView.swift
+++ MedicineNameQuiz_Sagae/View/Settings/SettingsListView.swift
@@ -5,22 +5,9 @@
 //  Created by 寒河江彪流 on 2023/09/30.
 //
 
-// 本アプリの第1回目のリリース目標として、ランキング機能なしで学習のみできるアプリを作成することとなりました。
-// そのため、第一段階のアプリ作成の間は、ランキング機能に関連するコードをコメントアウトします。
-
 import SwiftUI
 
 struct SettingsListView: View {
-    // サインインやユーザー名など、ランキング機能に関連するプロパティをコメントアウト
-    /*
-     // ユーザー名を管理する変数
-     @Binding var userName: String
-     // サインインしているかどうかを管理する変数
-     @Binding var isSignIn: Bool
-     // 初回のユーザー名設定画面の表示を管理する変数
-     @Binding var isFirstTimeUserNameSetting: Bool
-     */
-
     var body: some View {
         NavigationStack {
             // 奥から手前にレイアウト
@@ -46,15 +33,6 @@ struct SettingsListView: View {
                             case .advertisement:
                                 // 広告の表示について画面
                                 AdvertisementView(title: title)
-                            // ランキング機能が無ければ、アカウントは管理しないので、アカウント画面を表示するコードはコメントアウト
-                            /*
-                             case .account:
-                             // アカウント画面
-                             AccountView(isSignIn: $isSignIn,
-                             userName: $userName,
-                             isFirstTimeUserNameSetting: $isFirstTimeUserNameSetting,
-                             title: title)
-                             */
                             } // switch ここまで
                         } label: {
                             // 項目ごとに異なるテキストを表示
@@ -76,10 +54,5 @@ struct SettingsListView: View {
 } // SettingsListView ここまで
 
 #Preview {
-    SettingsListView(
-        // サインインやユーザー名など、ランキング機能に関連するプロパティをコメントアウト
-        /*
-         userName: .constant("sagae"), isSignIn: .constant(true), isFirstTimeUserNameSetting: .constant(false)
-         */
-    )
+    SettingsListView()
 }
